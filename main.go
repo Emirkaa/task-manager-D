b2895eb61693979805946cce2917dc566829e4f1
Emirkaa <xazzi.05@gmail.com>
2025-02-20 21:34:04 +0300

    update main.go

--- main.go
+++ main.go
@@ -1,27 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "github.com/jinzhu/gorm"
-import _"github.com/lib/pq"
-import "github.com/log"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 
+	"log"
+	"taskmanager/models"
+	"taskmanager/routes"
 
+	_ "github.com/lib/pq"
+)
 
 var db *gorm.DB
-func main(){
 
+func main() {
 
 	var ERR error
-	db, ERR = gorm.Open("postgres","user=emir","ssmode=disable","dbname=taskmanager")
-	if ERR != nil{
-		log.Fatal("Error: ",ERR)
+	db, ERR = gorm.Open("postgres", "user=emir", "ssmode=disable", "dbname=taskmanager")
+	if ERR != nil {
+		log.Fatal("Error: ", ERR)
 	}
-	defer db.close
-	
+	defer db.Close()
+
 	db.AutoMigrate(&models.Task{})
 	router := gin.Default()
-	SetupRouter(router)
+	routes.SetupRouters(router)
 	router.Run(":8080")
 
-	
 }
