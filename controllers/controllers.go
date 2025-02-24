f92f30c2c83f5e5cfa6ce0087db1516ac9d2efc1
Emirkaa <xazzi.05@gmail.com>
2025-02-24 21:06:12 +0300

    deletefunc

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -3,9 +3,17 @@ package controllers
 import (
 	"net/http"
 	"taskmanager/models"
+	"taskmanager/utills"
+	"taskmanager/task"
 
 	"github.com/gin-gonic/gin"
+	"net/http"
+	
+	
 )
+
+var taskrepo = task.TaskRepository
+
 
 func CreateTask(c *gin.Context) {
 	var j = models.Task{}
@@ -14,7 +22,36 @@ func CreateTask(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": error1.Error()})
 		return
 	}
+	
+	if j.Title == ""{
+		c.JSON(http.StatusBadRequest,gin.H("error":"Название задачи обязательно"))
+		return
+	}
 
 	c.JSON(http.StatusCreated, gin.H{"message": "created", "goal": j})
+
+}
+
+func GetTask(c *gin.Context){
+	id := c.Param("id")
+	j,err := task.GetByID(id)
+	if err != nil{
+		c.JSON(http.StatusBadRequest,gin.H{"error":"task not found"})
+		return
+	}
+	c.JSON(http.StatusOK,gin.H{"task":task})
+}
+
+func DeleteTask(c *gin.Context){
+	id := c.Param("id")
+	err := taskrepo.Delete("id")
+	if err != nil{
+		c.JSON(http.StatusNotFound, gin.H{"error":"id not found"})
+		return
+	}
+	c.JSON(http.StatusOK,gin.H{"message":"Task deleted"})
+}
+
+func PutTask(c *gin.Context){
 
 }
